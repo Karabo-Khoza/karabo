e2e32a4c0a1ff2bec9a18580556c466c3159515e
Karabo-Khoza <laurah.karabo@gmail.com>
2025-07-13 17:36:21 +0200

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,43 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temp = response.data.temperature.current;
   let cityElememnt = document.querySelector("#city");
+  let descriptionElememnt = document.querySelector("#description");
+  let humidityElememnt = document.querySelector("#humidity");
+  let windSpeedElememnt = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   temperatureElement.innerHTML = Math.round(temp);
+  descriptionElememnt.innerHTML = response.data.condition.description;
+  humidityElememnt.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElememnt.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
   cityElememnt.innerHTML = response.data.city;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
